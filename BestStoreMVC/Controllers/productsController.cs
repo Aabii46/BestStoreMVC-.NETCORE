00f431dd6d1e060e3d119911d9c7589562b743e4
Aabii46 <aabithmusharaf@gmail.com>
2024-12-23 17:33:43 +0530

    Add project files.

---
+++ BestStoreMVC/Controllers/productsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BestStoreMVC.Models;
+
+namespace BestStoreMVC.Controllers
+{
+    public class productsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public productsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.products.ToListAsync());
+        }
+
+        // GET: products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.products
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        // GET: products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Brand,Catagory,Price")] products products)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(products);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+
+        // GET: products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.products.FindAsync(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+            return View(products);
+        }
+
+        // POST: products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Brand,Catagory,Price")] products products)
+        {
+            if (id != products.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(products);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!productsExists(products.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+
+        // GET: products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.products
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        // POST: products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var products = await _context.products.FindAsync(id);
+            if (products != null)
+            {
+                _context.products.Remove(products);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool productsExists(int id)
+        {
+            return _context.products.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BestStoreMVC/Models/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BestStoreMVC.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base() 
+        { }
+        public DataContext (DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("A FALLBACK CONNECTION STRING");
+            }
+        }
+
+        public DbSet<products> products { get; set; }
+    }
+}
